25b3ca811a8b61faaaa0455bd713b67b1c73c587
Yoan marchal <marchalyoan@gmail.com>
2016-08-23 11:25:55 +0200

    fix no public deps

--- includes/class-address-plugin.php
+++ includes/class-address-plugin.php
@@ -155,9 +155,6 @@ class address_plugin
     private function define_public_hooks()
     {
         $plugin_public = new address_plugin_Public($this->get_address_plugin(), $this->get_version());
-
-        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
-        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
     }
 
     /**
